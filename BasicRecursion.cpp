820ad3841a4be2748b9cfe30df45dfd70bab8ac3
satwik <100366942+sAtM10@users.noreply.github.com>
2023-07-18 02:02:47 +0530

    Add files via upload

---
+++ BasicRecursion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int cnt = 0;
+void print(){
+    if(cnt==3) return;
+    cout<<cnt<< endl;
+    cnt++;
+
+    print();
+}
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
+    print();
+
+    return 0;
+}
