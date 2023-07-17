833df6082b008c5dfbf82e9731bbc0d81f43c68f
satwik <100366942+sAtM10@users.noreply.github.com>
2023-07-18 02:03:34 +0530

    Add files via upload

---
+++ Functions.cpp
@@ -0,0 +1,29 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void printName(string name){
+//     cout<<"Hey "<< name <<endl;
+// }
+// int main(){
+//     string name;
+//     cin >> name;
+//     printName(name);
+    
+//     return 0;
+// }
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int num1, int num2){
+    int num3 = num1 + num2;
+    return num3;
+
+}
+int main(){
+    int num1 ,num2;
+    cin>>num1>>num2;
+    int res=sum(num1,num2);
+    cout<<res;
+
+    return 0;
+}
